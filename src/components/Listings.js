4c6c6cb0d39157c20be68f9bb4f52c82220ee97d
Kabir Maan <kabirmaan9@gmail.com>
2018-07-13 19:00:16 +0100

    style filter section

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Listings extends Component {
   render() {
-    return <section className="listings">Listings</section>;
+    return (
+      <section className="listings">
+        <h4>Listings</h4>
+      </section>
+    );
   }
 }
 
