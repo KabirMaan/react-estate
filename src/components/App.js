6fed4d8e9f6335012730b07b5e482cd331fa3721
Kabir Maan <kabirmaan9@gmail.com>
2018-07-16 20:55:07 +0100

    Dummy data

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Filter from "./Filter";
 import Listings from "./Listings";
+import listingData from "../data/listingData";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listingData
+    };
+  }
   render() {
+    console.log(this.state.listingData);
     return (
       <div>
         <Header />
---
+++ src/data/listingData.js
@@ -0,0 +1,76 @@
+const listingData = [
+  {
+    address: "10 Winnington Road",
+    city: "London",
+    borough: "Camden",
+    rooms: 5,
+    price: 2000000,
+    floorspace: 1000,
+    extras: ["pool", "basement", "driveway"],
+    homeType: "house",
+    image:
+      "https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b749ef140950edd755a159baaca6b6f0&auto=format&fit=crop&w=1350&q=80"
+  },
+  {
+    address: "14 Wentworth Avenue",
+    city: "London",
+    borough: "Barnet",
+    rooms: 3,
+    price: 800000,
+    floorspace: 400,
+    extras: ["driveway", "basement"],
+    homeType: "house",
+    image:
+      "https://images.unsplash.com/photo-1494526585095-c41746248156?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fd170b4cebb0b97e6337529754defcf7&auto=format&fit=crop&w=1350&q=80"
+  },
+  {
+    address: "88 Ballards Lane",
+    city: "London",
+    borough: "Barnet",
+    rooms: 2,
+    price: 500000,
+    floorspace: 200,
+    extras: ["gym"],
+    homeType: "flat",
+    image:
+      "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7eb3cf84fa5988885f62b3ee0dfac1fe&auto=format&fit=crop&w=973&q=80"
+  },
+  {
+    address: "13 Finchley Road",
+    city: "London",
+    borough: "Barnet",
+    rooms: 3,
+    price: 750000,
+    floorspace: 450,
+    extras: ["pool", "gym"],
+    homeType: "flat",
+    image:
+      "https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d066245d73e72d59ca174847c8d844e0&auto=format&fit=crop&w=1347&q=80"
+  },
+  {
+    address: "10 Park View Road",
+    city: "London",
+    borough: "Barnet",
+    rooms: 3,
+    price: 850000,
+    floorspace: 500,
+    extras: ["driveway", "roof garden"],
+    homeType: "house",
+    image:
+      "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c7adacd8460808aaa0168f7cd060ec23&auto=format&fit=crop&w=800&q=80"
+  },
+  {
+    address: "2 Dale Avenue",
+    city: "London",
+    borough: "Camden",
+    rooms: 2,
+    price: 650000,
+    floorspace: 450,
+    extras: ["driveway", "basement"],
+    homeType: "house",
+    image:
+      "https://images.unsplash.com/photo-1504615755583-2916b52192a3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8a6b074881abc7cdc7abe977ce728ac9&auto=format&fit=crop&w=1267&q=80"
+  }
+];
+
+export default listingData;
